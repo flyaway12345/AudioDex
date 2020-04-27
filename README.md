59b415fef59df22ae7ee4667fb9e19d4d23a5b2b
Evan Haggard <evanhaggard123@gmail.com>
2020-04-27 15:20:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AudioDex
+# AudioDex
---
+++ script.js
@@ -0,0 +1,28 @@
+
+let goToShop = document.getElementById("shop");
+
+goToShop.addEventListener("click",function(){
+    window.open('https://himediamarketplace.com/products/pikachu-plushie', '_blank');
+});
+let signIn = document.getElementById("signIn");
+
+signIn.addEventListener("click",function(){
+    prompt("Input The Code Sent To Your Email");
+});
+let goToMailingList = document.getElementById("mailingList");
+
+goToMailingList.addEventListener("click",function(){
+    window.open('https://himediamarketplace.com/products/pikachu-plushie', '_blank');
+});
+
+let goToYoutube = document.getElementById("youtube");
+
+goToYoutube.addEventListener("click",function(){
+    window.open('https://www.youtube.com/playlist?list=PLrdsRuM2eycWwO9TTQCMWB2CvdcoPvl_X', '_blank');
+});
+
+let goToPokedex = document.getElementById("pokedex");
+
+goToPokedex.addEventListener("click",function(){
+    window.open('https://anchor.fm/audiodex', '_blank');
+});
